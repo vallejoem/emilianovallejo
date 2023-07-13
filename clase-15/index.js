b295667f4ab1cccf975412b7705e05fc1d0e6c58
vallejoem <emiliano.vallejo@gmail.com>
2023-07-13 16:27:54 -0300

    clase 15 terminado

--- clase-15/index.js
+++ clase-15/index.js
@@ -10,33 +10,31 @@ agregar.addEventListener("click", function(){
     const btn = document.createElement("button");
     btn.innerHTML="Eliminar";
     const node =  document.createTextNode(texto);
-    //const parrafo = document.createElement("p");
-    //elementoDiv.textContent="texto";
-    divRow.id="divrow";
+    divRow.id=texto;
     elementoDiv.id="elementId";
     elementoDiv1.id="elementId1";
     btn.id="btnId";
-    //parrafo.id="parrafoid";
-    //parrafo.textContent=texto;
-    //parrafo.appendChild("parrafo");
-    //div1.appendChild("texto");
-    //div1.after("elementoDiv");
-    divRow.appendChild(elementoDiv, elementoDiv1);
-    document.body.appendChild(divRow);
-    divRow.setAttribute("class", "row");
+    divRow.appendChild(elementoDiv);
+    divRow.appendChild(elementoDiv1);
+    div1.appendChild(divRow);
+    divRow.setAttribute("class", "row justify-content-center ");
     elementoDiv.appendChild(node);
     divRow.appendChild(elementoDiv);
-    elementoDiv.setAttribute("class", "bg-primary col-6 text-white text-center py-1 ");
+    elementoDiv.setAttribute("class", "bg-primary col-4 text-white text-center py-1 border-end border-white items");
     elementoDiv1.appendChild(btn);
     divRow.appendChild(elementoDiv1);
-    elementoDiv1.setAttribute("class", "bg-primary col-6 text-center py-1");
-    btn.setAttribute("class", "bg-dark text-white rounded px-3 py-1");
+    elementoDiv1.setAttribute("class", "bg-primary col-4 text-center py-1 items");
+    btn.setAttribute("class", "bg-dark text-white rounded px-5 py-1");
+    btn.addEventListener("click", function(){
+        const deleteElement = document.getElementById(texto);
+        div1.removeChild(deleteElement);
+    });
+    const divAll = document.querySelectorAll('.items');
+    divAll[2].setAttribute('class', 'bg-primary-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
+    divAll[3].setAttribute('class', 'bg-primary-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
+    divAll[4].setAttribute('class', 'bg-primary-border-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
+    divAll[5].setAttribute('class', 'bg-primary-border-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
+  console.log(elementoDiv.getAttribute('class'));
 });
 
-
-
-
-//const elementoDiv = document.createElement("div");
-//elementoDiv.textContent="Agregando Items";
-//elementoDiv.classList.add("classone");
 
