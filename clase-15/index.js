8386e9586f01e501d47eb76130af3482e2772a6b
vallejoem <emiliano.vallejo@gmail.com>
2023-07-19 09:42:22 -0300

    terminado

--- clase-15/index.js
+++ clase-15/index.js
@@ -20,21 +20,25 @@ agregar.addEventListener("click", function(){
     divRow.setAttribute("class", "row justify-content-center ");
     elementoDiv.appendChild(node);
     divRow.appendChild(elementoDiv);
-    elementoDiv.setAttribute("class", "bg-primary col-4 text-white text-center py-1 border-end border-white items");
+    elementoDiv.setAttribute("class", " col-4 text-white text-center py-1 border-end border-white items");
     elementoDiv1.appendChild(btn);
     divRow.appendChild(elementoDiv1);
-    elementoDiv1.setAttribute("class", "bg-primary col-4 text-center py-1 items");
+    elementoDiv1.setAttribute("class", " col-4 text-center py-1 items");
     btn.setAttribute("class", "bg-dark text-white rounded px-5 py-1");
     btn.addEventListener("click", function(){
         const deleteElement = document.getElementById(texto);
         div1.removeChild(deleteElement);
     });
-    const divAll = document.querySelectorAll('.items');
+    /*const divAll = document.querySelectorAll('.items');
     divAll[2].setAttribute('class', 'bg-primary-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
     divAll[3].setAttribute('class', 'bg-primary-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
     divAll[4].setAttribute('class', 'bg-primary-border-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
     divAll[5].setAttribute('class', 'bg-primary-border-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
-  console.log(elementoDiv.getAttribute('class'));
+    divAll[8].setAttribute('class', 'bg-primary-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
+    divAll[9].setAttribute('class', 'bg-primary-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
+    divAll[10].setAttribute('class', 'bg-primary-border-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
+    divAll[11].setAttribute('class', 'bg-primary-border-subtle col-4 text-primary-emphasis text-center py-1 border-end border-white items');
+  console.log(elementoDiv.getAttribute('class'));*/
 });
 
 
